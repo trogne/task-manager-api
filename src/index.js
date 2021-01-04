78567266c0f620228cf135773bbaa910ed88b022
Patrick Fiset <patrick.fiset@gmail.com>
2021-01-03 22:47:35 -0500

    add get miko

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT
 
+// my add
+app.get('/miko', (req,res) => {
+    console.log('AMAZING MIKO')
+    res.send('SUPER MIKO')
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
